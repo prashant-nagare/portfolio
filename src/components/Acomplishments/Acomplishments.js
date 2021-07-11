c98869a23f7016319a275d22908cc09ece42205b
Prashant Nagare <nagarepk@gmail.com>
2021-07-11 10:07:03 +0530

    Update Acomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,10 +4,10 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 50, text: 'Shell Scripts', },
-  { number: 200000, text: 'Downloads', },
-  { number: 100, text: 'Github Stars', }
+  { number: 05, text: 'Open Source Projects'},
+  { number: 12, text: 'Shell Scripts', },
+  { number: 50000, text: 'Downloads', },
+  { number: 4, text: 'Github Stars', }
 ];
 
 const Acomplishments = () => (
