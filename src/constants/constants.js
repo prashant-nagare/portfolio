da0d337b42c867059dc6683c96d0aeba56d2bc56
Prashant Nagare <nagarepk@gmail.com>
2022-11-06 00:11:12 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,15 +9,6 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'Covid-19 Status App',
-    description: "COVID-19 Status is a free, open-source Android application that shows the current state of COVID-19 in India and the world. It has a beautiful, simple, and fast UI.",
-    image: '/images/covid.jpg',
-    tags: ['Java', 'XML', 'Firebase', 'Rest API'],
-    source: 'https://github.com/Coders-Of-XDA-OT/covid19-status-android',
-    visit: 'https://project.vipuljha.com/covid/',
-    id: 0,
-  },
-  {
     title: 'Portfolio Website',
     description: "This is my personal portfolio website that has all my work and project experience, including my resume. It was my main portfolio before.",
     image: '/images/portfolio.jpg',
